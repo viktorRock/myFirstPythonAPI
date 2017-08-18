a2e9355614b18b464557edde9337dcb49c48d59a
PAULO VICTOR MATHIAS SILVA <pvmathiassilva@gmail.com>
2017-08-18 17:04:53 -0300

    heroku test

---
+++ pythonapi/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url, include
+from pythapp import views
+from rest_framework import routers, serializers, viewsets
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
--- pythonapi/wsgi.py
+++ pythonapi/wsgi.py
@@ -1,15 +1,16 @@
 """
-WSGI config for gettingstarted project.
+WSGI config for pythonapi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pythonapi.settings")
 
 application = get_wsgi_application()
