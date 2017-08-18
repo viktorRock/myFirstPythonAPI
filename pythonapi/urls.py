54f8a8b1e1257befa546c186e99e4edfc96a1bcd
PAULO VICTOR MATHIAS SILVA <pvmathiassilva@gmail.com>
2017-08-18 20:20:24 -0300

    heroku test

--- pythonapi/urls.py
+++ pythonapi/urls.py
@@ -14,4 +14,5 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    , url(r'^digitalParrot', views.index, name='index')
 ]
