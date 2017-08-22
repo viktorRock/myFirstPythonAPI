aabaaff1c6e7391755ccac5f79d80cd0ef137946
PAULO VICTOR MATHIAS SILVA <pvmathiassilva@gmail.com>
2017-08-21 22:29:44 -0300

    heroku test

--- pythapp/views.py
+++ pythapp/views.py
@@ -14,10 +14,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
-
-for user in User.objects.all():
-    Token.objects.get_or_create(user=user)
-
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
@@ -62,3 +58,6 @@ def index(request):
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+for user in User.objects.all():
+    Token.objects.get_or_create(user=user)
